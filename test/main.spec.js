e2e0d46046acba058113df017d37735de15ce0c5
James Drew <j.drew1303@gmail.com>
2016-10-05 13:15:03 +0000

    generate api

--- test/main.spec.js
+++ test/main.spec.js
@@ -5,4 +5,8 @@ describe('basic tests', () => {
     it('displays the version', () => {
         expect(sqlgenerate.version).to.be.equal(require('../package.json').version);
     });
+    
+    it('contains the generate api', () => {
+        expect(sqlgenerate.generate).to.be.a('function');
+    })
 });
