2170f7b9fa90c47e8f7419e2599ea52eed4facf5
James Drew <j.drew1303@gmail.com>
2016-10-05 13:07:26 +0000

    rename version test.

--- test/main.spec.js
+++ test/main.spec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import sqlgenerate from '..';
 
 describe('basic tests', () => {
-    it('should be able to display the version', () => {
+    it('displays the version', () => {
         expect(sqlgenerate.version).to.be.equal(require('../package.json').version);
-    })
+    });
 });
