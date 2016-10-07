58e312e472f2ab1bf2755a4c20cd326bb04b1a3a
James Drew <j.drew1303@gmail.com>
2016-10-07 17:24:25 +0000

    started working on the test suite
    the entire suite is autogenerated from the test cases provided to it.
    adding a new squite involves creating a subfolder in the sql folder
    and adding an sql statement. The next time the test suite is run it
    will create a describe and it block for the new statement. this does
    however mean that there are 65 failing tests and currently only a
    single passing test.

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,13 +1,55 @@
-import { expect } from 'chai';
-import sqlgenerate from '..';
+import { expect }   from 'chai';
+import {generate}   from '..';
+import { readFile } from 'fs';
+import parser       from 'sqlite-parser';
+import glob         from 'glob';
+import * as _       from 'lodash';
 
-xdescribe('basic tests', () => {
-    it('displays the version', () => {
-        expect(sqlgenerate.version).to.be.equal(require('../package.json').version);
-    });
+describe('test suite', () => {
+    // get all sql files in our test suite
+    let files = glob.sync('**/*.sql');
     
-    it('contains the generate api', () => {
-        expect(sqlgenerate.generate).to.be.a('function');
-    });
+    // extract information from the filepath names
+    let tests = _.groupBy(_.map(files, (file) => {
+        let r = /^test\/sql\/([\s\S]*)\/([\s\S]*)\.sql/gi.exec(file);
+        return {
+            filePath : file,
+            group : r[1], 
+            name : r[2]
+        };
+    }), 'group');
     
+    // extract a list of the main areas under test
+    let groups = _.keys(tests);
+
+    // we dynamically generate the test suites from the above information
+    _.map(groups, (group) => {
+        
+        describe(`${group} statements`, () => {
+            
+            _.map(tests[group], (test) => {
+                it('generates ' + test.name, (done) => {
+                    readFile(test.filePath, (err, data) => {
+                        if (err) throw "Unable to read file";
+                        
+                        var sql = data.toString();
+                        var ast = parser(sql);
+                        
+                        var regeneratedSQL = generate(ast);
+                        
+                        var isParsible = parser(regeneratedSQL)
+                        
+                        expect(isParsible).to.deep.equal(ast);
+                        expect(standardiseString(sql)).to.equal(standardiseString(regeneratedSQL));
+                        done();
+                    });
+                });
+            });
+        });
+    });
 });
+
+function standardiseString(str){
+  return str.replace(/\s+/g, '')
+            .toLowerCase();
+}
