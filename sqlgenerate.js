51a7fbb578a7688bc667c64086381f2c1e53ce33
James Drew <j.drew1303@gmail.com>
2016-10-07 01:28:32 +0000

    fix linting issues

--- sqlgenerate.js
+++ sqlgenerate.js
@@ -1,17 +1,19 @@
+'use strict';
+
 var sqltraverse = require('sqltraverse');
-var generateExpression = require('./src/expressions')
+var generateExpression = require('./src/expressions');
 var _ = require('lodash');
 
 function generateSelect (selectNode) {
     
-    var result = selectNode.result.map((result) => {return result.name}).join(', ');
-    var from = selectNode.from.map((from) => {return from.name}).join(', ');
+    var result = selectNode.result.map(function (result) {return result.name;}).join(', ');
+    var from = selectNode.from.map(function (from) {return from.name;}).join(', ');
     var where = generateExpression(_.first(selectNode.where));
     
-    return `SELECT ${result} FROM ${from} WHERE ${where}`
+    return `SELECT ${result} FROM ${from} WHERE ${where}`;
 }
 
-var generate = function (ast) {
+function generate (ast) {
     return ast.statement.map(generateSelect).join('\n');
 }
 
